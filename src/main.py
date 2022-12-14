d397ccb029e32f09182bd7b0d23b21c12556803e
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2022-12-14 20:58:50 +0530

    Add window and main game loop

--- src/main.py
+++ src/main.py
@@ -1,4 +1,24 @@
+import pygame
+
 def main():
-    pass
+    pygame.init()
+
+    background_colour = (0, 0, 0)
+    (width, height) = (800, 600)
+
+    screen = pygame.display.set_mode((width, height))
+    pygame.display.set_caption("Tic Tac Toe")
+
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        screen.fill(background_colour)
+        pygame.display.flip()
+
+    pygame.quit()
+
 if __name__ == '__main__':
     main()
