7b7702228627043a08aef82834f4df72b37c3481
Nabeel Siddiqui <nabeel.siddiqui.314@gmail.com>
2022-12-18 21:13:26 +0530

    Rename function parameter

--- src/gui/text.py
+++ src/gui/text.py
@@ -2,15 +2,15 @@ import pygame
 
 
 class Text:
-    def __init__(self, text, font, position, color):
+    def __init__(self, text, font, center, color):
         self.text = text
         self.font = font
         self.color = color
         self.textSurface = self.font.render(text, True, color)
 
-        positionX, positionY = position
+        centerX, centerY = center
         sizeX, sizeY = self.textSurface.get_size()
-        self.drawPosition = (positionX - sizeX / 2, positionY - sizeY / 2)
+        self.drawPosition = (centerX - sizeX / 2, centerY - sizeY / 2)
 
     def render(self, screen):
         screen.blit(self.textSurface, self.drawPosition)
